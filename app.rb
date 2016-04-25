10c47a41e812db8c86c7ecb898040a49011fd2c8
Ben Stein <bumstain@gmail.com>
2016-04-26 01:22:30 +1000

    Initialized shotgun and defined new cat route

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do 
+  "This is a secret message."
+end
+
+get '/page2' do 
+  "This is another page."
+end
+
+get '/cat' do 
+  "<div style='border: medium dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
